51b64cf32f5f5d770f631df57cc05df1aaf77049
BlueTot <bluetotkc@gmail.com>
2025-02-16 22:58:01 +0000

    working on raw input/output, step 31 complete

--- kilo.c
+++ kilo.c
@@ -6,38 +6,43 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+
+/*** Defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** Data ***/
 
-// the original terminal settings
-struct termios orig_termios;
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** Terminal ***/
 
-/**
- * Function to print error message
- */
+/* Function to print error message and exit */
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear the screen
+    write(STDOUT_FILENO, "\x1b[H", 3); // move cursor to start
     perror(s);
     exit(1);
 }
 
-/**
- * Function to disable raw mode for the text editor, upon exiting
- */
+/* Function to disable raw mode for the text editor, upon exiting */
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
         die("tcsetattr");
     }
 }
 
-/**
- * Function to enable raw mode for the text editor, upon entering
- */
+/* Function to enable raw mode for the text editor, upon entering */
 void enableRawMode() {
 
     // get terminal settings and check for error
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
         die("tcgetattr");
     }
 
@@ -45,7 +50,7 @@ void enableRawMode() {
     atexit(disableRawMode);
 
     // make new terminal setting set to previous setting
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     // set the flags for raw mode
     raw.c_iflag &= ~(BRKINT | ICRNL | IXON | INPCK | ISTRIP);
@@ -61,21 +66,107 @@ void enableRawMode() {
     }
 }
 
-/*** Init ***/
-int main() {
-    enableRawMode();
-
-    while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+/* Function to read a key from the user */
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
             die("read");
         }
+    }
+    return c;
+}
+
+/* Function to get the cursor position */
+int getCursorPosition(int *rows, int *cols) {
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+        return -1;
+    }
+
+    printf("\r\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+    }
+
+    editorReadKey();
+
+    return -1;
+}
+
+/* Function to get the window size
+ * Parameters: rows - pointer to int storing num of rows
+ *             cols - pointer to int storing num of cols
+ * Return: -1 if failed, 0 otherwise
+ */
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+            return -1;
+        }
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** Output ***/
+
+/* Function to draw rows to the screen */
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+         write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+/* Function to refresh the screen */
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4); // clear the screen
+    write(STDOUT_FILENO, "\x1b[H", 3); // move cursor to start
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3); // move cursor to start
+}
+
+/*** Input ***/
+
+/* Function to process key from user */
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+/*** Init ***/
+
+/* Start up the editor by initialising the struct */
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+        die("getWindowSize");
+    }
+}
+
+/* Main function */
+int main() {
+    enableRawMode();
+    initEditor();
+
+    while (1) {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
