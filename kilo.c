409fffa8f7313a81bcd15367a50c8cb54a5bec4f
BlueTot <bluetotkc@gmail.com>
2025-04-23 11:57:49 +0100

    added showing non-printable characters, completed step 176

--- kilo.c
+++ kilo.c
@@ -859,10 +859,21 @@ void editorDrawRows(struct abuf *ab) {
             int current_color = -1;
             int j;
             for (j = 0; j < len; j++) {
-                // if the character has normal color
-                if (hl[j] == HL_NORMAL) {
-                    // if the previous color is not normal
+                if (iscntrl(c[j])) { // if the character is a control character
+                    char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+                    abAppend(ab, "\x1b[7m", 4);
+                    abAppend(ab, &sym, 1);
+                    abAppend(ab, "\x1b[m", 3);
                     if (current_color != -1) {
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm",
+                                            current_color);
+                        abAppend(ab, buf, clen);
+                    }
+                } else if (hl[j] ==
+                           HL_NORMAL) { // if the character has normal color
+                    if (current_color !=
+                        -1) { // if the previous color is not normal
                         abAppend(ab, "\x1b[39m", 5); // clear color
                         current_color = -1;
                     }
