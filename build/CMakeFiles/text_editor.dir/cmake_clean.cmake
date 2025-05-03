cf765d26186503c21cf9193d8d6ecf41b795036b
Nok Hang Lo <bluetotkc@gmail.com>
2025-05-03 10:14:09 +0100

    changed to use cmake to work with nvim lsp

---
+++ build/CMakeFiles/text_editor.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_editor.dir/src/append_buffer.c.o"
+  "CMakeFiles/text_editor.dir/src/append_buffer.c.o.d"
+  "CMakeFiles/text_editor.dir/src/data.c.o"
+  "CMakeFiles/text_editor.dir/src/data.c.o.d"
+  "CMakeFiles/text_editor.dir/src/editor_ops.c.o"
+  "CMakeFiles/text_editor.dir/src/editor_ops.c.o.d"
+  "CMakeFiles/text_editor.dir/src/file_io.c.o"
+  "CMakeFiles/text_editor.dir/src/file_io.c.o.d"
+  "CMakeFiles/text_editor.dir/src/find.c.o"
+  "CMakeFiles/text_editor.dir/src/find.c.o.d"
+  "CMakeFiles/text_editor.dir/src/input.c.o"
+  "CMakeFiles/text_editor.dir/src/input.c.o.d"
+  "CMakeFiles/text_editor.dir/src/log.c.o"
+  "CMakeFiles/text_editor.dir/src/log.c.o.d"
+  "CMakeFiles/text_editor.dir/src/main.c.o"
+  "CMakeFiles/text_editor.dir/src/main.c.o.d"
+  "CMakeFiles/text_editor.dir/src/output.c.o"
+  "CMakeFiles/text_editor.dir/src/output.c.o.d"
+  "CMakeFiles/text_editor.dir/src/row_ops.c.o"
+  "CMakeFiles/text_editor.dir/src/row_ops.c.o.d"
+  "CMakeFiles/text_editor.dir/src/syntax.c.o"
+  "CMakeFiles/text_editor.dir/src/syntax.c.o.d"
+  "CMakeFiles/text_editor.dir/src/terminal.c.o"
+  "CMakeFiles/text_editor.dir/src/terminal.c.o.d"
+  "CMakeFiles/text_editor.dir/src/yank_buffer.c.o"
+  "CMakeFiles/text_editor.dir/src/yank_buffer.c.o.d"
+  "text_editor"
+  "text_editor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/text_editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
